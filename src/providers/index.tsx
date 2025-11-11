0f794a1b5412abb02b816d7cf4f4a66024cb324a
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2025-11-11 21:58:10 +0700

    set up shadcn

---
+++ src/providers/index.tsx
@@ -0,0 +1,12 @@
+import { Toaster } from "@/components/ui/sonner";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      {children}
+      <Toaster
+        richColors
+        position="top-center" />
+    </>
+  )
+}
